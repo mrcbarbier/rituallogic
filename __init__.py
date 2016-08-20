a43fda3177619e72fb593489436a3e387c954447
MRCBarbier <contact@mrcbarbier.org>
2016-08-20 15:21:07 +0200

    UI fix

--- __init__.py
+++ __init__.py
@@ -8,40 +8,14 @@ from utilityclasses import *
 
 
 if __name__ =='__main__':
-    parser=IOHandler()
-    sequences=parser.parse('example.dat')
-    gviz=GraphVisualization()
-    seqview=SequenceViewer()
-    for seq in sequences:
-        graphs=seqview.frame_by_frame(seq)
-        for g in graphs:
-            gviz.plot(g,hold=1)
-        plt.show()
+    from ui import *
+    app = QtGui.QApplication(sys.argv)
+    myapp = Editor()
+    #myapp.set_data_from('example.dat')
 
-if 0:
+    myapp.show()
+    sys.exit(app.exec_())
 
-
-    y=Node("yaj",atoms=('human') )
-    f=Node("fire",atoms=('unlit') )
-
-    a=Action('y_light_f',atoms=('slowly'),members={'AGENT':y,'OBJECT':f},
-         changes={f:AtomChange('-unlit +lit')} )
-
-    fr1=Frame('initial')
-    fr2=Frame('sutra1',actions=[a])
-
-    s=Sequence('DPM',frames=[fr1,fr2])
-
-
-    atom_ont=Ontology('atom')
-    atom_ont.add_edge('slowly','speed','IN')
-    atom_ont.add_edge('quickly','speed','IN')
-    atom_ont.add_edge('speed','manner','IN')
-
-    q=QueryHandler()
-    gviz=GraphVisualization()
-
-
-    print s.frames[0].nodes
-
-    gviz.plot(atom_ont,hold=0)
+    if 0:
+        gviz=GraphVisualization()
+        gviz.plot(myapp.parser.atom_db,hold=0)
